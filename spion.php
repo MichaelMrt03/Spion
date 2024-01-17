001b851f2eb42507da4ce505dcff24ac5c31620b
Aman Ur-Rehman <urrehman.aman@outlook.de>
2024-01-17 23:25:27 +0100

    spionauswahl und namensabfrage hinzugefügt

--- spion.php
+++ spion.php
@@ -8,6 +8,26 @@ $anzahlSpieler = 4;
 
 #Auswahl welches Wort
 $wort = random_int(0,$anzahlWorte);
+
+#Auswahl wer Spion wird
+$spion = random_int(0,$anzahlSpieler)
+
+#Eingabe der Namen
+print("
+    <form action=‘spielstart.php?spion=$spion\‘ method=‘POST‘>
+        <input type=‘text‘ placeholder=‘spieler1‘ name=‘spieler1‘ ><br><br>
+        <input type=‘text‘ placeholder=‘spieler2‘ name=‘spieler2‘ ><br><br>
+        <input type=‘text‘ placeholder=‘spieler3‘ name=‘spieler3‘ ><br><br>
+        <input type=‘text‘ placeholder=‘spieler4‘ name=‘spieler4‘ ><br><br>
+        <button type=‘submit‘>Spielstart!</button>
+    </form>
+");
+
+
+#Ansatz für variable Anzahl an Spielern
+// for($i;$i<$anzahlSpieler;$i=0){
+//     print("<input type=‘text‘ name=‘$spieler[]‘ ><br><br>");
+
 #Auswahl wer Spion wird
 
 #Ausgabe der Wörter/Spion 
