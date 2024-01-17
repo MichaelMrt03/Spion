c7d77a12c3d02d427ddd9d774a6ea52f77000e49
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-01-17 22:05:15 +0100

    Variablen hinzugefügt

--- spion.php
+++ spion.php
@@ -1,11 +1,13 @@
 <?php
-#Wörterliste
-$worte = ["Hochaus","Meer","Telefon"]
+#Variablen
+$worte = ["Hochaus","Meer","Telefon"]; #Wörterliste
+$anzahlWorte = count($worte);
+$anzahlSpieler = 4;
 
 #Startbutton
 
 #Auswahl welches Wort
-
+$wort = random_int(0,$anzahlWorte);
 #Auswahl wer Spion wird
 
 #Ausgabe der Wörter/Spion 
