d7d677d48d6e326c56dde9514d319dc07d1001e8
Aman Ur-Rehman <urrehman.aman@outlook.de>
2024-01-18 20:56:36 +0100

    variable Anzahl an Spielern

--- spion.php
+++ spion.php
@@ -1,29 +1,41 @@
 <?php
-#Variablen
-$worte = ["Hochaus","Meer","Telefon"]; #Wörterliste
-$anzahlWorte = count($worte);
-$anzahlSpieler = 4;
-
-#Startbutton
-
-#Auswahl welches Wort
-$wort = random_int(0,$anzahlWorte);
-
-#Eingabe der Namen
+#Anzahl an Spielern
 print("
-    <form action=‘spielstart.php‘ method=‘POST‘>
-        <input type=‘text‘ placeholder=‘spieler1‘ name=‘spieler1‘ ><br><br>
-        <input type=‘text‘ placeholder=‘spieler2‘ name=‘spieler2‘ ><br><br>
-        <input type=‘text‘ placeholder=‘spieler3‘ name=‘spieler3‘ ><br><br>
-        <input type=‘text‘ placeholder=‘spieler4‘ name=‘spieler4‘ ><br><br>
-        <button type=‘submit‘>Spielstart!</button>
+    <form action='spion.php' method='POST'>
+        <input type='number' placeholder='Spieleranzahl hier eingeben...' name='spieleranzahl' ><br><br>
+        <button type='submit'>OK</button>
     </form>
 ");
 
 
+
 #Ansatz für variable Anzahl an Spielern
-// for($i;$i<$anzahlSpieler;$i=0){
-//     print("<input type=‘text‘ name=‘$spieler[]‘ ><br><br>");
+if(isset($_POST["spieleranzahl"])){
+    print("<form action='spielstart.php' method='POST'>");
+    $anzahlSpieler = $_POST["spieleranzahl"];
+    for($i=0;$i<$anzahlSpieler;$i++){
+        $j=$i+1;
+        $placeholder="Name für Spieler ".$j." eingeben...";
+        print("<input type='text' placeholder=$placeholder name='$i\' ><br><br>");
+    }
+
+    print("
+            <button type='submit'>OK</button>
+        </form>"
+    );
+}
+
+
+#Eingabe der Namen
+// print("
+//     <form action='spielstart.php' method='POST'>
+//         <input type='text' placeholder='spieler1' name='spieler1' ><br><br>
+//         <input type='text' placeholder='spieler2' name='spieler2' ><br><br>
+//         <input type='text' placeholder='spieler3' name='spieler3' ><br><br>
+//         <input type='text' placeholder='spieler4' name='spieler4' ><br><br>
+//         <button type='submit'>Spielstart!</button>
+//     </form>
+// ");
 
 #Auswahl wer Spion wird
 
