55b28597e2b7b8e6198bf08ba7a4c898d1a2976b
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-01-18 23:58:55 +0100

    Gliederung in 3 Formuulare Formular1->spieleranzahl Formular2->Namen Formular3->Ausgabe der Spieler

--- spion.php
+++ spion.php
@@ -13,7 +13,7 @@ if (!isset($_POST["spieleranzahl"])) {
 
 #Ansatz für variable Anzahl an Spielern
 #Zweites Formular
-if (isset($_POST["spieleranzahl"])) {
+if (isset($_POST["spieleranzahl"]) && !isset($_POST["spielername0"])) {
     print("<form action='spion.php' method='POST'>");
     $anzahlSpieler = $_POST["spieleranzahl"];
     for ($i = 0; $i < $anzahlSpieler; $i++) {
@@ -21,21 +21,26 @@ if (isset($_POST["spieleranzahl"])) {
         $placeholder = "Name für Spieler " . $spieler_nr . " eingeben...";
         print("<input type='text' placeholder=$placeholder name='spielername$i' ><br><br>");
     }
+    #Spieleranzahl für Formular2 sichern
+    print(" <input type='hidden' name='spieleranzahl' value='$anzahlSpieler'>");
+
     print("
             <button type='submit'>OK</button>
         </form>"
     );
-
-    #Wenn die Spielernamen & Spieleranzahl gesetzt wurden Werden die Spieler ausgegeben
-    for ($i = 0; $i < $anzahlSpieler; $i++) {
-        if (isset($_POST["spielername$i"])) {
-            print($_POST["spielername$i"] . "<br>");
-            print($_POST);
-        }else{print("not set");}
-    }
 }
 
-print($anzahlSpieler);
+#Formular3
+if (isset($_POST["spieleranzahl"]) && isset($_POST["spielername0"])) {
+    #Wenn die Spielernamen & Spieleranzahl gesetzt wurden, werden die Spieler ausgegeben
+    for ($i = 0; $i < $_POST["spieleranzahl"]; $i++) {
+        if (isset($_POST["spielername$i"])) {
+            print($_POST["spielername$i"] . "<br>");
+        } else {
+            print("not set");
+        }
+    }
+}
 
 #Eingabe der Namen
 // print("
